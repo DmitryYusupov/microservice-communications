5e1b035884634bc5996127ece5bdf3966ade54b5
dyusupov <dmitry.yusupov@infobip.com>
2020-10-14 12:01:54 +0300

    initial commit

---
+++ src/main/kotlin/ru/yusdm/spike/interaction/InteractionApplication.kt
@@ -0,0 +1,11 @@
+package ru.yusdm.spike.interaction
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InteractionApplication
+
+fun main(args: Array<String>) {
+	runApplication<InteractionApplication>(*args)
+}
---
+++ src/test/kotlin/ru/yusdm/spike/interaction/InteractionApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.yusdm.spike.interaction
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InteractionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
